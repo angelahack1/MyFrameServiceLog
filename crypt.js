c50b479650d8b30bf1408743b3605f106150325d
angelahack1 <angelakimichellle@hotmail.com>
2024-05-18 06:27:19 -0600

    Just some stuff added to crypt sample, by angelahack1

--- crypt.js
+++ crypt.js
@@ -1,7 +1,7 @@
 const forge = require('node-forge');
 const fs = require('fs');
 
-// Read the private key
+// Read the public key
 const pem = fs.readFileSync('./cert.pem', 'utf8');
 const certificate = forge.pki.certificateFromPem(pem);
 const publicKey = certificate.publicKey;
@@ -21,4 +21,24 @@ const encrypted = publicKey.encrypt(forgeBuffer.getBytes(), 'RSA-OAEP');
 // Convert encrypted data to base64
 const encryptedBase64 = forge.util.encode64(encrypted);
 
-console.log(encryptedBase64);
+console.log("Encrypted: <<<"+encryptedBase64+">>>");
+console.log("");
+
+//Read the private key
+const pem2 = fs.readFileSync('./key.pem', 'utf8');
+const key = forge.pki.privateKeyFromPem(pem2);
+
+//Data to be decrypted
+const decoded = forge.util.decode64(encryptedBase64);
+
+//Convert data to buffer
+const bufferEnc = Buffer.from(decoded, 'binary');
+
+//COnvert buffer to forge buffer
+const forgeBufferEnc = forge.util.createBuffer(bufferEnc.toString('binary'));
+
+//Decrypt data
+const decrypted = key.decrypt(forgeBufferEnc.getBytes(), 'RSA-OAEP');
+
+console.log("Decrypted: <<<"+decrypted+">>>");
+
