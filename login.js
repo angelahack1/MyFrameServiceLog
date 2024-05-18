ca5ff4520a618622473bcb294f6c524497f60ac4
angelahack1 <angelakimichellle@hotmail.com>
2024-05-18 04:47:41 -0600

    Just adding some secure login stuff sample, by angelahack1

---
+++ login.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const basicAuth = require('express-basic-auth')
+
+const portNumber = 9090;
+
+// Initialize express app
+const app = express();
+
+// Simple access control mechanism
+app.use(basicAuth({
+    users: { 'angelahack1': '1164Loud' }, // Replace with your desired username and password
+    challenge: true
+}))
+
+app.get('/', (req, res) => {
+    res.send("Hello, secure Angy's Wortld!!!!");
+});
+
+// SSL/TLS certificate and key
+const options = {
+    key: fs.readFileSync('./key.pem'), // Path to your key file
+    cert: fs.readFileSync('./cert.pem') // Path to your certificate file
+};
+
+https.createServer(options, app).listen(portNumber, () => {
+    console.log('Secure server listening on port: '+portNumber);
+});
