5a7927fdac7c636296b1dbb5b03fd1f033dbcee5
angelahack1 <angelakimichellle@hotmail.com>
2024-05-22 01:51:47 -0600

    Just adding secure layer (https) to index.js, by angelahack1

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const basicAuth = require('express-basic-auth')
+
+const portNumber = 443;
+
 const app = express();
 app.use(express.json());
 
 let items = ['Hola', ' ', 'como', ' ', 'estas', ' ', 'Angy...' ];
+
+app.use(basicAuth({
+    users: { 'angelahack1': 'myPassHacked' },
+    challenge: true
+}));
+
+app.get('/', (req, res) => {
+    res.send("Hello, secure Angy's World!!!!");
+});
 
 app.get('/items', (req, res) => {
     res.json(items);
@@ -25,6 +40,12 @@ app.delete('/items/:id', (req, res) => {
     res.send();
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// SSL/TLS certificate and key
+const options = {
+    key: fs.readFileSync('./key2.pem'), // Path to your key file
+    cert: fs.readFileSync('./cert2.pem') // Path to your certificate file
+};
 
+https.createServer(options, app).listen(portNumber, () => {
+    console.log('Secure server listening on port: '+portNumber);
+});
